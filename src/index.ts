3f74056063a41c0ffbe95e15c8a663a880d92aa1
Charlie Karniol <charliekarniol@gmail.com>
2020-02-12 21:33:22 +0200

    Refer instead of destructuring API components

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,17 @@ import { time } from './time';
 interface ASimpleCache extends Cache, Memoization, Statistics {};
 
 const cache: ASimpleCache = {
-    ...Cache,
-    ...Memoization,
-    ...Statistics,
+    set: Cache.set,
+    get: Cache.get,
+    has: Cache.has,
+    isValid: Cache.isValid,
+    delete: Cache.delete,
+    keys: Cache.keys,
+    clear: Cache.clear,
+    memoize: Memoization.memoize,
+    invalidate: Memoization.invalidate,
+    enableStatistics: Statistics.enableStatistics,
+    statistics: Statistics.statistics,
 };
 
 export {
